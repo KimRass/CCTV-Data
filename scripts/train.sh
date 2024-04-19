ca6d28aac434e9a043e05a4dceb13a5495b7d21a
KimRass <rmx1000@yonsei.ac.kr>
2024-04-19 16:38:26 +0900

    Training

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,1 +1,1 @@
-yolo task=detect mode=train model=yolov8n.pt data=/home/jbkim/Desktop/workspace/CCTV-Data/cctv.yaml epochs=15 imgsz=512 plots=True workers=4 project=CCTV-stroller
+yolo task=detect mode=train model=yolov8n.pt data=/home/jbkim/Desktop/workspace/CCTV-Data/cctv.yaml epochs=15 imgsz=640 plots=True workers=4 project=CCTV-stroller
